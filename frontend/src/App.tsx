510646a55670beb2c6633eb26e88537f9d274eb7
Mijahla <smijahla@gmail.com>
2024-08-10 20:18:08 +0545

    added edit hotel feature and added e2e tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ import SignIn from "./pages/SignIn";
 import AddHotel from "./pages/AddHotel";
 import { useAppContext } from "./contexts/AppContext";
 import MyHotels from "./pages/MyHotels";
+import EditHotel from "./pages/EditHotel";
 
 
 const App = () => {
@@ -48,6 +49,12 @@ const App = () => {
         </Layout>
          }
        />
+       <Route path="/edit-hotel/:hotelId" element={
+        <Layout>
+          <EditHotel />
+        </Layout>
+         }
+         />
        <Route path="/my-hotels" element={
         <Layout>
           <MyHotels />
